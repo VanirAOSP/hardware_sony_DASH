0347d497e47c02d4af51208f8c03c9864fa70215
Adrian DC <radian.dc@gmail.com>
2015-06-09 22:43:50 +0200

    Sensors - AK8963 Update

--- libs/akm8963/Android.mk
+++ libs/akm8963/Android.mk
@@ -1,12 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 
+ifeq ($(SOMC_CFG_SENSORS_COMPASS_AK8963),yes) 
 include $(CLEAR_VARS)
-
-ifneq ($(SOMC_CFG_SENSORS_AKM8963_DUMMY),)
-
+LOCAL_SRC_FILES += ak8963_dummy.c
 LOCAL_MODULE := libsensors_akm8963
 LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES += ak8963_dummy.c
 include $(BUILD_SHARED_LIBRARY)
-
 endif
